9263ba0a8376a85fe4aefebb7d90a93ba0eb55dd
Смольков Владислав Андреевич <smolkovva@cifra-k.ru>
2026-06-10 02:46:17 +0700

    feat: v1

--- auth/myapp/application/services/pass_manager.py
+++ auth/myapp/application/services/pass_manager.py
@@ -16,3 +16,36 @@ class PasswordManager(IPasswordManager):
 
     async def hash_password(self, password: str) -> str:
         return self.pwd_context.hash(password)
+
+
+
+
+
+@pytest.mark.django_db
+class AuthApiTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.factory = APIRequestFactory()
+        cache.clear() # Очищаем кэш между тестами
+        self.creator = UserCreator()
+        self.candidate = self.creator.create_user(UserRole.CANDIDATE)
+
+    def _post_register(self, data: dict) -> Response:
+        view = AuthApi.as_view({"post": "register"}, permission_classes=[AllowAny])
+        request = self.factory.post(
+            path="/api/core/auth/register",
+            data=data,
+            format="json")
+        return view(request=request)
+
+    def test_user_create_with_exist_email(self) -> None:
+        """Тест регистрации пользователя с уже существующим email."""
+        existing_email = user_with_exist_email_data["email"]
+        user_in_db = self.candidate
+        user_in_db.email = existing_email
+        user_in_db.save()
+
+        response = self._post_register(data=user_with_exist_email_data)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "email" in response.data
